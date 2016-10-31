e22e4b62ae894af2d8826035af7922a139dcf50b
anpreg <annamaria.preg@capgemini.com>
2016-10-31 15:17:25 +0100

    Send In correspondence initial commit

---
+++ SendInCorrespondence/SendInCorrespondence/SendInCorrespondenceDal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using SendInCorrespondence.ICorrespondenceAgencyExternalBasic;
+using System.Configuration;
+
+namespace SendInCorrespondence
+{
+    public class SendInCorrespondenceDal
+    {
+        public static void InsertCorrespondence(string systemUserName, string systemPassword, string systemUserCode,
+            string externalShipmentReferece, InsertCorrespondenceV2 correspondence)
+        {
+            ReceiptExternal receipt;
+
+            try
+            {
+                using (CorrespondenceAgencyExternalBasicClient client = new CorrespondenceAgencyExternalBasicClient())
+                {
+                    receipt = client.InsertCorrespondenceBasicV2(systemUserName, systemPassword, systemUserCode, externalShipmentReferece, correspondence);
+                    if (receipt.ReceiptStatusCode == ReceiptStatusEnum.OK)
+                        return;
+
+                    throw new Exception($"Error while sending correspondence: {receipt.ReceiptStatusCode}, {receipt.ReceiptText}");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public static InsertCorrespondenceV2  CreateCorrespondence()
+        {
+            InsertCorrespondenceV2 correspondence = new InsertCorrespondenceV2();
+            correspondence.ServiceCode = ConfigurationManager.AppSettings["serviceCode"];
+            correspondence.ServiceEdition = ConfigurationManager.AppSettings["serviceEdition"];
+            correspondence.Reportee = ConfigurationManager.AppSettings["orgNo"];
+            correspondence.VisibleDateTime = DateTime.Parse(ConfigurationManager.AppSettings["visibleDateTime"]);
+            correspondence.AllowSystemDeleteDateTime = DateTime.Parse(ConfigurationManager.AppSettings["allowSystemDeleteDateTime"]);
+            correspondence.DueDateTime = DateTime.Parse(ConfigurationManager.AppSettings["dueDateTime"]);            
+
+            correspondence.Content = new ExternalContentV2
+                {
+                    LanguageCode = ConfigurationManager.AppSettings["languageCode"],
+                    MessageTitle = "Title",
+                    MessageSummary = "Summary text",
+                    MessageBody = "Body text",
+                }
+            ;
+
+            correspondence.Notifications = new NotificationBEList
+            {
+                new Notification1
+                {
+                    FromAddress = "no-reply@altinn.no",
+                    ShipmentDateTime = DateTime.Parse(ConfigurationManager.AppSettings["shipmentDateTime"]),
+                    LanguageCode = ConfigurationManager.AppSettings["languageCode"],
+                    NotificationType = ConfigurationManager.AppSettings["notificationTemplate"],
+                    ReceiverEndPoints = new ReceiverEndPointBEList
+                    {
+                        new ReceiverEndPoint
+                        {
+                            TransportType = TransportType.Email,
+                            ReceiverAddress = ConfigurationManager.AppSettings["epost"],
+                        },
+                        new ReceiverEndPoint
+                        {
+                            TransportType = TransportType.SMS,
+                            ReceiverAddress = ConfigurationManager.AppSettings["mobilePhone"],
+                        }
+                    }
+                }
+            };            
+
+            return correspondence;
+        }        
+    }
+}
---
+++ SendInCorrespondence/SendInCorrespondenceConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SendInCorrespondence;
+using System.Configuration;
+using SendInCorrespondence.ICorrespondenceAgencyExternalBasic;
+
+
+namespace SendInCorrespondenceConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertCorrespondenceV2 correspondence = SendInCorrespondenceDal.CreateCorrespondence();
+            SendInCorrespondenceDal.InsertCorrespondence(
+                ConfigurationManager.AppSettings["systemUserName"],
+                ConfigurationManager.AppSettings["systemPassword"],
+                ConfigurationManager.AppSettings["systemUserCode"],
+                Guid.NewGuid().ToString(),
+                correspondence);               
+        }        
+    }
+}
