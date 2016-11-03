68e03ad87b98bd7fceb67a48bc65b0989a73afc2
anpreg <annamaria.preg@capgemini.com>
2016-11-03 13:16:51 +0100

    Errorhandling

--- IntegrationAMelding/Program.cs
+++ IntegrationAMelding/Program.cs
@@ -20,14 +20,19 @@ namespace IntegrationAMelding
             //03057900961
             //EINAR NORHEIM
             //910253492
-            var pins = new [] { "ajhhs", "piksd", "iuyhs", "asdfg", "rtefs", "loj7s", "mmmyp", "juksa", "fizck", "qicks", "98ujs", "mnbvs", "qwers", "polze", "ztang", "alt1n", "zcatt", "kjasd", "23as3", "lkiju", "4564s", "zxhfg", "alsks", "ooiks", "likme", "kaffe", "arbei", "00kks", "mjhgg", "ziste"};
+            var pins = new[]
+            {
+                "ajhhs", "piksd", "iuyhs", "asdfg", "rtefs", "loj7s", "mmmyp", "juksa", "fizck", "qicks", "98ujs",
+                "mnbvs", "qwers", "polze", "ztang", "alt1n", "zcatt", "kjasd", "23as3", "lkiju", "4564s", "zxhfg",
+                "alsks", "ooiks", "likme", "kaffe", "arbei", "00kks", "mjhgg", "ziste"
+            };
 
             //create and submit FormTask
             int receiptId = SubmitFormTask();
             Console.WriteLine($"Formtask created and sent. ReceiptId: {receiptId}");
 
             //get archive reference
-            string archiveReference = GetArchiveReference(receiptId);           
+            string archiveReference = GetArchiveReference(receiptId);
             Console.WriteLine($"Archive reference: {archiveReference}");
 
             int pinIndex = GetPinIndexForAuthentication();
@@ -66,37 +71,47 @@ namespace IntegrationAMelding
                     DataFormatId = ConfigurationManager.AppSettings["DataFormatId"],
                     DataFormatVersion = int.Parse(ConfigurationManager.AppSettings["DataFormatVersion"]),
                     FormData = ConfigurationManager.AppSettings["formData"],
-                    EndUserSystemReference = "uniqueReferenceee" + DateTime.Now.ToLongTimeString(),                    
+                    EndUserSystemReference = "uniqueReferenceee" + DateTime.Now.ToLongTimeString(),
                     Completed = true
                 };
 
                 var formList = new Form[1];
                 formList[0] = form;
-                ft.Forms = formList;                
+                ft.Forms = formList;
                 fts.FormTasks = ft;
 
-                var receipt = client.SubmitFormTaskBasic(ConfigurationManager.AppSettings["systemUserName"], ConfigurationManager.AppSettings["systemPassword"], null, null, null, null, fts);
+                var receipt = client.SubmitFormTaskBasic(ConfigurationManager.AppSettings["systemUserName"],
+                    ConfigurationManager.AppSettings["systemPassword"], null, null, null, null, fts);
                 return receipt.ReceiptId;
             }
         }
 
         private static string GetArchiveReference(int receiptId)
         {
-            using (var client = new ReceiptExternalBasicClient())
+            try
             {
-                var receiptSearch = new ReceiptSearchExternal
+                using (var client = new ReceiptExternalBasicClient())
                 {
-                    ReceiptId = receiptId,
-                    References = new Receipt.ReferenceList()
-                };
+                    var receiptSearch = new ReceiptSearchExternal
+                    {
+                        ReceiptId = receiptId,
+                        References = new Receipt.ReferenceList()
+                    };
 
-                var receipt = client.GetReceiptBasic(ConfigurationManager.AppSettings["systemUserName"], ConfigurationManager.AppSettings["systemPassword"], receiptSearch);
+                    var receipt = client.GetReceiptBasic(ConfigurationManager.AppSettings["systemUserName"],
+                        ConfigurationManager.AppSettings["systemPassword"], receiptSearch);
 
-                foreach (var reference in receipt.References)
-                {
-                    if (reference.ReferenceTypeName == Receipt.ReferenceType.ArchiveReference)
-                        return reference.ReferenceValue;
+                    foreach (var reference in receipt.References)
+                    {
+                        if (reference.ReferenceTypeName == Receipt.ReferenceType.ArchiveReference)
+                            return reference.ReferenceValue;
+                    }
                 }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Failed to get archive reference: {exception.Message}");
+                throw;
             }
 
             return string.Empty;
@@ -104,79 +119,103 @@ namespace IntegrationAMelding
 
         private static int GetPinIndexForAuthentication()
         {
-            using (var client = new SystemAuthenticationExternalClient())
+            try
             {
-                AuthenticationChallengeRequestBE challengeRequest = new AuthenticationChallengeRequestBE
+                using (var client = new SystemAuthenticationExternalClient())
                 {
-                    SystemUserName = ConfigurationManager.AppSettings["systemUserName"],
-                    UserSSN = ConfigurationManager.AppSettings["ssn"],
-                    AuthMethod = "AltinnPin"
-                };
+                    AuthenticationChallengeRequestBE challengeRequest = new AuthenticationChallengeRequestBE
+                    {
+                        SystemUserName = ConfigurationManager.AppSettings["systemUserName"],
+                        UserSSN = ConfigurationManager.AppSettings["ssn"],
+                        AuthMethod = "AltinnPin"
+                    };
 
-                var challenge = client.GetAuthenticationChallenge(challengeRequest);
-                if (challenge.Status == ChallengeRequestResult.Ok)
-                {
-                    var digits = challenge.Message.SkipWhile(c => !char.IsDigit(c))
-                                 .TakeWhile(char.IsDigit)
-                                 .ToArray();
+                    var challenge = client.GetAuthenticationChallenge(challengeRequest);
+                    if (challenge.Status == ChallengeRequestResult.Ok)
+                    {
+                        var digits = challenge.Message.SkipWhile(c => !char.IsDigit(c))
+                            .TakeWhile(char.IsDigit)
+                            .ToArray();
 
-                    var str = new string(digits);
-                    return int.Parse(str);
+                        var str = new string(digits);
+                        return int.Parse(str);
+                    }
                 }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Failed to get pin index for authentication: {exception.Message}");
+                throw;
             }
 
             return -1;
         }
 
         private static ReporteeElementBEV2 GetCorrespondence(string pin, string archieveReference)
-        {            
-            using (var client = new ReporteeElementListExternalBasicClient())
+        {
+            try
             {
-                ExternalSearchBEV2 searchList = new ExternalSearchBEV2
+                using (var client = new ReporteeElementListExternalBasicClient())
                 {
-                    Reportee = ConfigurationManager.AppSettings["orgNo"],
-                    ToDate = DateTime.Now
-                };
+                    ExternalSearchBEV2 searchList = new ExternalSearchBEV2
+                    {
+                        Reportee = ConfigurationManager.AppSettings["orgNo"],
+                        ToDate = DateTime.Now
+                    };
 
 
-                var correspondenceList = client.GetReporteeElementListBasicV2(
-                    ConfigurationManager.AppSettings["systemUserName"],
-                    ConfigurationManager.AppSettings["systemPassword"],
-                    ConfigurationManager.AppSettings["ssn"],
-                    ConfigurationManager.AppSettings["systemPassword"],
-                    pin, "AltinnPin",
-                    searchList,
-                    int.Parse(ConfigurationManager.AppSettings["languageCode"]));
+                    var correspondenceList = client.GetReporteeElementListBasicV2(
+                        ConfigurationManager.AppSettings["systemUserName"],
+                        ConfigurationManager.AppSettings["systemPassword"],
+                        ConfigurationManager.AppSettings["ssn"],
+                        ConfigurationManager.AppSettings["systemPassword"],
+                        pin, "AltinnPin",
+                        searchList,
+                        int.Parse(ConfigurationManager.AppSettings["languageCode"]));
 
-                return correspondenceList.FirstOrDefault(c => c.ArchiveReference == archieveReference);
+                    return correspondenceList.FirstOrDefault(c => c.ArchiveReference == archieveReference);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Failed to get correspondence: {exception.Message}");
+                throw;
             }
         }
 
         private static void ArchiveCorrespondence(string pin, ReporteeElementBEV2 correspondence)
         {
-            using (var client = new CorrespondenceExternalBasicClient())
+            try
             {
-                //var correspondenceForEUS = client.GetCorrespondenceForEndUserSystemsBasicV2(
-                //    ConfigurationManager.AppSettings["systemUserName"],
-                //    ConfigurationManager.AppSettings["systemPassword"],
-                //    ConfigurationManager.AppSettings["ssn"],
-                //    ConfigurationManager.AppSettings["systemPassword"],
-                //    pin, "AltinnPin",
-                //    correspondence.SEReporteeElementID,
-                //    int.Parse(ConfigurationManager.AppSettings["languageCode"]));
-
-                var receipt = client.ArchiveCorrespondenceForEndUserSystemBasic(
-                    ConfigurationManager.AppSettings["systemUserName"],
-                    ConfigurationManager.AppSettings["systemPassword"],
-                    ConfigurationManager.AppSettings["ssn"],
-                    ConfigurationManager.AppSettings["systemPassword"],
-                    pin, "AltinnPin",
-                    correspondence.SEReporteeElementID);
-
-                if (receipt.ReceiptStatusCode != ReceiptStatusEnum.OK)
+                using (var client = new CorrespondenceExternalBasicClient())
                 {
-                    throw new InvalidOperationException($"{receipt.ReceiptStatusCode} : {receipt.ReceiptText}");
-                }                    
+                    //var correspondenceForEUS = client.GetCorrespondenceForEndUserSystemsBasicV2(
+                    //    ConfigurationManager.AppSettings["systemUserName"],
+                    //    ConfigurationManager.AppSettings["systemPassword"],
+                    //    ConfigurationManager.AppSettings["ssn"],
+                    //    ConfigurationManager.AppSettings["systemPassword"],
+                    //    pin, "AltinnPin",
+                    //    correspondence.SEReporteeElementID,
+                    //    int.Parse(ConfigurationManager.AppSettings["languageCode"]));
+
+                    var receipt = client.ArchiveCorrespondenceForEndUserSystemBasic(
+                        ConfigurationManager.AppSettings["systemUserName"],
+                        ConfigurationManager.AppSettings["systemPassword"],
+                        ConfigurationManager.AppSettings["ssn"],
+                        ConfigurationManager.AppSettings["systemPassword"],
+                        pin, "AltinnPin",
+                        correspondence.SEReporteeElementID);
+
+                    if (receipt.ReceiptStatusCode != ReceiptStatusEnum.OK)
+                    {
+                        throw new InvalidOperationException($"{receipt.ReceiptStatusCode} : {receipt.ReceiptText}");
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Failed to get correspondence: {exception.Message}");
+                throw;
             }
         }
     }
